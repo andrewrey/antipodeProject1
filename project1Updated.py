f7c0bde76b04535424305107a470fe3752a9a620
Andrew Reynolds <andrew_reynolds@mac.com>
2021-03-10 16:19:52 -0800

    created additionaly python file using the syntactic sugar for calling methods

---
+++ project1Updated.py
@@ -0,0 +1,35 @@
+from folium import Map, Marker
+ 
+# Get Latitude and Longitude. As well set the tool tip message.
+latitude = 40.09
+longitude = -3.47
+tool_tip = "Click Me!!"
+
+# reverse the latitude to the opposite
+antipode_latitude = latitude * -1 
+
+# Logice for reversing Longitude: -180 if postive, +180 if negative
+if longitude < 0.0 and longitude > -180.0:
+    antipode_longitude = longitude + 180.0
+elif longitude == 0.0:
+    antipode_longitude = 180.0
+elif longitude > 180.0 or longitude < -180.0:
+    antipode_longitude = f"Error {longitude}: check number"
+else:
+    antipode_longitude = longitude - 180.0
+    
+# create a list of the antipodes for Lat and Long    
+location = [antipode_latitude, antipode_longitude]
+
+# Create the Map using Stamen Terrain, add location marker and popup with tooltip
+mymap = Map(location,tiles="Stamen Terrain")
+Marker(location, popup=f"<i>My Antipode: Latitude {antipode_latitude} Longitude {antipode_longitude} </i>", tooltip=tool_tip).add_to(mymap)
+
+# Save map to file
+mymap.save("antipode2.html")
+
+
+print(antipode_latitude, antipode_longitude)
+print(mymap)
+
+

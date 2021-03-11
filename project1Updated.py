c81b0cdcca7d60a98bfc1c5a24d9a5a7f8d05f73
Andrew Reynolds <andrew_reynolds@mac.com>
2021-03-10 20:35:18 -0800

    added info icon for starting point on map

--- project1Updated.py
+++ project1Updated.py
@@ -1,10 +1,11 @@
-from folium import Map, Marker
+from folium import Map, Marker, Icon
 from geo import Geopoint
  
 # Get Latitude and Longitude. As well set the tool tip message.
-latitude = 40.09
-longitude = -3.47
+latitude = 22.32
+longitude = 114.17
 tool_tip = "Click Me!!"
+starting_point = "Starting Point!!"
 
 # reverse the latitude to the opposite
 antipode_latitude = latitude * -1 
@@ -25,6 +26,7 @@ location = [antipode_latitude, antipode_longitude]
 # Create the Map using Stamen Terrain, add location marker and popup with tooltip
 mymap = Map(location,tiles="Stamen Terrain")
 Marker(location, popup=f"<i>My Antipode: Latitude {antipode_latitude} Longitude {antipode_longitude} </i>", tooltip=tool_tip).add_to(mymap)
+Marker(location=[latitude, longitude], popup=f'<i>Starting Point:Latitude {latitude}, Longitude {longitude}</i>', tooltip=starting_point, icon=Icon(color="red", icon="info-sign")).add_to(mymap)
 
 # Save map to file
 mymap.save("antipode2.html")

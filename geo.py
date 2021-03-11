638c598d35b9a582d2fc153d526ea6f3dd799234
Andrew Reynolds <andrew_reynolds@mac.com>
2021-03-10 23:08:47 -0800

    added method for getting local time

--- geo.py
+++ geo.py
@@ -1,3 +1,8 @@
+import datetime as dt
+from pytz import timezone
+from timezonefinder import TimezoneFinder
+
+
 class Geopoint:
     
     def __init__(self,latitude, longitude):
@@ -6,4 +11,18 @@ class Geopoint:
         
     def closest_parallel(self):
         return round(self.latitude)
+    
+    def get_time(self):
+        tf = TimezoneFinder()
+        time_zone_string = tf.timezone_at(lat=self.latitude, lng=self.longitude)
+        source_date = dt.datetime.now(timezone(f'{time_zone_string}'))
+        return source_date
+
+    
         
+        
+van = Geopoint(49.28, -123.12)
+
+print(van.closest_parallel())
+
+print(van.get_time())

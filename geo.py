327905b81d6618a4d144856d06495f3cd4f487f5
Andrew Reynolds <andrew_reynolds@mac.com>
2021-03-14 16:34:39 -0700

    added class method

--- geo.py
+++ geo.py
@@ -3,9 +3,11 @@ from pytz import timezone
 from timezonefinder import TimezoneFinder
 from sunnyday import Weather
 from config import api_key
-
+from random import uniform
 
 class Geopoint:
+    latitude_range = (90,-90)
+    longitude_range = (180, -180)
     
     def __init__(self,latitude, longitude):
         self.latitude = latitude
@@ -23,11 +25,17 @@ class Geopoint:
     def get_weather(self):
         weather = Weather(api_key, lat=self.latitude, lon=self.longitude)
         return weather.next_12h_simplified()
-
+    
+    @classmethod
+    def get_random(cls):
+        return cls(latitude=uniform(-90, 90), longitude=uniform(-180, 180))
+    
     
         
         
 van = Geopoint(49.28, -123.12)
+random_point = Geopoint.get_random()
+print(random_point)
 
 print(van.closest_parallel())
 

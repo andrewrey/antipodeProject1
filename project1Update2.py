5704e8a89f981507ba2ef31bdaa16789efc258b8
Andrew Reynolds <andrew_reynolds@mac.com>
2021-03-16 15:58:17 -0700

    updated layout of popup

--- project1Update2.py
+++ project1Update2.py
@@ -12,7 +12,12 @@ mymap = Map(location = [latitude, longitude])
 
 # Create a Geopoint instance
 geopoint = Geopoint(latitude, longitude)
-pop_up = Popup(html='<h2 style="color:blue">{}</h2>'.format(geopoint.get_weather()[0][2]))
+pop_up = Popup(html=
+               """<div style="width: 200px; display: flex; align-items: center; justify-content: space-between">
+                      <h2 style="margin: 0; padding:0">{}</h2>
+                      <img src="http://openweathermap.org/img/wn/{}@2x.png" style="width: 50px">
+                  </div>
+               """.format(geopoint.get_weather()[0][2], geopoint.get_weather()[0][3]))
 pop_up.add_to(geopoint)
 geopoint.add_to(mymap)
                     

3d8ea6eed632579090984cd9bfc29c09c52a020c
Andrew Reynolds <andrew_reynolds@mac.com>
2021-03-16 17:41:17 -0700

    added loop to get_weather function to print out the hourly forecast

--- project1Update2.py
+++ project1Update2.py
@@ -2,8 +2,8 @@ from folium import Map, Marker, Icon, Popup
 from geo import Geopoint
 
 # Get latitude and Longitude values
-latitude = 40.4
-longitude = -3.7
+latitude = 35.67
+longitude = 139.65
 
 
 #Folium Map instance
@@ -12,12 +12,21 @@ mymap = Map(location = [latitude, longitude])
 
 # Create a Geopoint instance
 geopoint = Geopoint(latitude, longitude)
-pop_up = Popup(html=
-               """<div style="width: 200px; display: flex; align-items: center; justify-content: space-between">
-                      <h2 style="margin: 0; padding:0">{}</h2>
-                      <img src="http://openweathermap.org/img/wn/{}@2x.png" style="width: 50px">
-                  </div>
-               """.format(geopoint.get_weather()[0][2], geopoint.get_weather()[0][3]))
+weather = geopoint.get_weather()
+html = """<div style="width: 200px; display: flex; flex-direction: column">"""
+
+for hourly in weather:
+    html += f"""
+        <div style="border-bottom: 1px solid black; display:flex; align-items:center">
+            <h2 style="padding:0;margin:0">{hourly[0][-8:-6]}H {round(hourly[1])}&deg;</h2>
+            <img src="http://openweathermap.org/img/wn/{hourly[3]}@2x.png" style="width: 50px">
+        </div>
+    """
+    
+html += "</div>"
+    
+
+pop_up = Popup(html= html)
 pop_up.add_to(geopoint)
 geopoint.add_to(mymap)
                     

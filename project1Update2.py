ff62d09155fd43e5fef8e0677c7d88c4dd8b9833
Andrew Reynolds <andrew_reynolds@mac.com>
2021-03-16 21:26:23 -0700

    used for loop to add multiple markers

--- project1Update2.py
+++ project1Update2.py
@@ -6,29 +6,33 @@ latitude = 35.67
 longitude = 139.65
 
 
+# Locations List
+locations = [[48,120], [55, 178], [-22, -110]]
+
 #Folium Map instance
 mymap = Map(location = [latitude, longitude])
 
 
-# Create a Geopoint instance
-geopoint = Geopoint(latitude, longitude)
-weather = geopoint.get_weather()
-html = """<div style="width: 200px; display: flex; flex-direction: column">"""
+for loc in locations:
+    # Create a Geopoint instance
+    geopoint = Geopoint(latitude=loc[0], longitude=loc[1])
+    weather = geopoint.get_weather()
+    html = """<div style="width: 200px; display: flex; flex-direction: column">"""
 
-for hourly in weather:
-    html += f"""
-        <div style="border-bottom: 1px solid black; display:flex; align-items:center">
-            <h2 style="padding:0;margin:0">{hourly[0][-8:-6]}H {round(hourly[1])}&deg;</h2>
-            <img src="http://openweathermap.org/img/wn/{hourly[3]}@2x.png" style="width: 50px">
-        </div>
-    """
-    
-html += "</div>"
-    
+    for hourly in weather:
+        html += f"""
+            <div style="border-bottom: 1px solid black; display:flex; align-items:center">
+                <h2 style="padding:0;margin:0">{hourly[0][-8:-6]}H {round(hourly[1])}&deg;</h2>
+                <img src="http://openweathermap.org/img/wn/{hourly[3]}@2x.png" style="width: 50px">
+            </div>
+        """
+        
+    html += "</div>"
+        
 
-pop_up = Popup(html= html)
-pop_up.add_to(geopoint)
-geopoint.add_to(mymap)
+    pop_up = Popup(html= html)
+    pop_up.add_to(geopoint)
+    geopoint.add_to(mymap)
                     
 
 
